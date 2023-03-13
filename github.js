19e1ad818065c46bb55b36b192445ff09e602b26
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-13 15:54:06 +0600

    Infos are displaying well

--- github.js
+++ github.js
@@ -1,12 +1,53 @@
+
+
 const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getUser("kabirshuvo");
 
 async function getUser(user){
     const resp = await fetch(APIURL + user);
     const respData = await resp.json();
 
+    console.log(respData);
+
     createUserCard(respData);
 }
 
 function createUserCard(user) {
+    
+        const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avater" src="${user.avatar_url}" alt="${user.name}"/>
+            </div>
+            <div>
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+                <ul class="info">
+                    <li>${user.followers}</li>
+                    <li>${user.following}</li>
+                    <li>${user.public_repos}</li>
+                </ul>
+            </div>
+        </div>
         
+        `;
+
+            main.innerHTML = cardHTML;
 }
+
+            form.addEventListener('submit', e => {
+            e.preventDefault();
+
+            const user = search.value;
+
+            if(user){
+                getUser(user);
+                search.value = '';
+            }
+            });
