af22a0ebcb5944bac33b64268deb66a74efaa4c1
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-13 16:28:30 +0600

    Card is looking much better now

--- github.js
+++ github.js
@@ -21,17 +21,17 @@ function createUserCard(user) {
     
         const cardHTML = `
         <div class="card">
-            <div>
+            <div class="image-container">
                 <img class="avater" src="${user.avatar_url}" alt="${user.name}"/>
             </div>
-            <div>
+            <div class="user-info">
                 <h2>${user.name}</h2>
                 <p>${user.bio}</p>
 
                 <ul class="info">
-                    <li>${user.followers}</li>
-                    <li>${user.following}</li>
-                    <li>${user.public_repos}</li>
+                    <li> <strong>Followers</strong> ${user.followers}</li>
+                    <li> <strong>Following</strong> ${user.following}</li>
+                    <li> <strong>Repos</strong> ${user.public_repos}</li>
                 </ul>
             </div>
         </div>
